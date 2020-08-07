4e749871488583403faa67c602c680c2f5e34b70
sachinp-git <sachin@buildops.com>
2020-08-07 16:17:39 +0530

    Add command files

---
+++ database-setup.sh
@@ -0,0 +1,5 @@
+npx prisma migrate save --experimental
+#! Select ‘yes’ for the prompt to add a file db this command and enter a name for #the migration. 
+npx prisma migrate up --experimental
+npx prisma generate
+
